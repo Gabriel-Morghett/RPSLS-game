37f00aca304acffe7eb738eda78c8e70097d466e
Gabriel Morghett <gabriel.morghett@gmail.com>
2024-02-17 21:55:40 -0300

    added style and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from './routes/Home';
 import Result from './routes/Result'
+import Header from './components/Header';
 
 
 
 function App() {
 	return (
     	<div className="App">
+			<Header/>
       		<Router>
         		<Routes>
 					<Route path={"/"} element={<Home />} />
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,21 +1,37 @@
+import Container from "../components/Container"
+import versus from '../img/versus.jpg'
 
 export default function Home() {
 
     return (
-        <div>
-            <form>
-                <input 
-                    type="text" 
-                    placeholder="Player 1" 
-                    onChange={(e) => console.log("1")} 
-                />
-                <input 
-                    type="text" 
-                    placeholder="Prayer 2" 
-                    onChange={(e) => console.log("2")} 
-                />
-                <button type="submit">Play</button>
-            </form>
+        <>
+        <div className="game">
+            <Container>
+                <form>
+                    <input 
+                        type="text" 
+                        placeholder="Player 1" 
+                        onChange={(e) => console.log("1")} 
+                    />
+                </form>
+            </Container>
+
+            <img src={versus} className='img-versus'/>
+
+            <Container>
+                <form>      
+                    <input 
+                        type="text" 
+                        placeholder="Prayer 2" 
+                        onChange={(e) => console.log("2")} 
+                    />
+                </form>
+            </Container>
         </div>
+            <Container>
+                <button type="submit">Play</button>
+            </Container>
+        </>
+        
     )
 }
